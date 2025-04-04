020d07a7172bd13a30d54de9b7ff35a7069944b2
Sia <mail@siavush.co.uk>
2025-04-04 10:33:10 +0100

    Random agent refined

--- REINFORCE.py
+++ REINFORCE.py
@@ -1,9 +1,10 @@
 import torch
+import numpy as np
 import gymnasium as gym
+import matplotlib.pyplot as plt
 from torch import nn
 
 # Hyperparameters
-STEPS = 200
 ROLLOUTS = 200
 
 # This is a Mac implementation
@@ -13,43 +14,53 @@ class Agent(nn.Module):
     def __init__(self):
         super().__init__()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(16, 128),
+            nn.Linear(4, 128),
             nn.ReLU(),
             nn.Linear(128, 128),
             nn.ReLU(),
-            nn.Linear(128, 4)
+            nn.Linear(128, 2)
         )
 
-    def forward(self, state):
+    def forward(self, state, deterministic=True):
         logits = self.linear_relu_stack(state)
-        return logits
+        if deterministic:
+            action = int(torch.argmax(logits))
+        return logits, action 
 
 
 if __name__ == "__main__":
 
     # Setup up environment
     env = gym.make("CartPole-v1") 
-    next_obs, info = env.reset()
     
     # Create agent
     agent = Agent().to(device)
 
     # Storage
-    actions = []
-    rewards = []
-    observations = []
+    rollout_rewards = np.zeros(ROLLOUTS)
 
-    for step in range(STEPS):
-        observations.append(next_obs)
-        action = env.action_space.sample()
-        next_obs, reward, terminated, truncated, info = env.step(action)
-        rewards.append(reward)
-        actions.append(int(action))
+    for rollout in range(ROLLOUTS):
+        
+        # Reset at beginning of rollouts
+        total_reward = 0
+        next_obs, info = env.reset()
 
-        if terminated or truncated:
-            next_obs, info = env.reset()
+        done = False
+        while not done:
+            with torch.no_grad():
+                obs_tensor = torch.FloatTensor(next_obs).to(device)
+                logits, action = agent(obs_tensor)
+
+            next_obs, reward, terminated, truncated, info = env.step(action)
+            total_reward += reward
+            done = terminated or truncated
+
+        rollout_rewards[rollout] = total_reward
 
     env.close()
-    print(rewards)
-    print(actions)
-
+    
+    # Print results
+    plt.plot(rollout_rewards) 
+    plt.xlabel("Rollout")
+    plt.ylabel("Total Reward")
+    plt.show()
