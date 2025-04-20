e229ce147020e66a7ef840145fa98eb98062525e
Sia <mail@siavush.co.uk>
2025-04-20 12:54:14 +0100

    Added device_setup func

--- policy_gradient_algorithm_performance/vanilla_policy_gradient/VPG.py
+++ policy_gradient_algorithm_performance/vanilla_policy_gradient/VPG.py
@@ -1,10 +1,21 @@
+# File: VPG.py
+# Created: 19 April 2025
+# ------------------------------
+# Summary
+# ------------------------------
+# This file runs the vanilla policy gradient (VPG) algorith.
+# 
+
+
 # TODO: Add annealing
 # TODO: Add baseline
 # TODO: Entropy regularisation
 # TODO: Reward normalisation
 
+
 import torch
 import argparse
+import platform
 import gymnasium as gym
 from torch import nn
 
@@ -15,16 +26,19 @@ ENV = "FrozenLake-v1"
 ALPHA = 1e-3
 GAMMA = 0.99
 ROLLOUTS = 500
+HIDDEN_IN = 128
+HIDDEN_OUT = 128
+
 
 class Network(nn.Module):
-    def __init__(self):
+    def __init__(self, observation_space, action_space):
         super().__init__()
         self.linear_relu_stack = nn.Sequential(
-                nn.Linear(STATE_DIM, HIDDEN_IN),
+                nn.Linear(observation_space, HIDDEN_IN),
                 nn.ReLU(),
                 nn.Linear(HIDDEN_IN, HIDDEN_OUT),
                 nn.ReLU(),
-                nn.Linear(HIDDEN_OUT, ACTION_DIM)
+                nn.Linear(HIDDEN_OUT, action_space)
                 )
 
         def forward(self, state):
@@ -32,6 +46,7 @@ class Network(nn.Module):
             probs = Categorical(logits=logits)
             action = probs.sample()
             return probs.log_prob(action), action
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Vanilla Policy Gradient Algorithm")
@@ -67,8 +82,25 @@ def parse_args():
     return parser.parse_args()
 
 
+def setup_device():
+    system = platform.system()
+
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
+
+    return device
+
+
 def main():
     args = parse_args()
+    device = setup_device()
+    env = None
+    agent = None
+
 
 if __name__ == "__main__":
     main()
--- policy_gradient_algorithm_performance/vanilla_policy_gradient/test_VPG.py
+++ policy_gradient_algorithm_performance/vanilla_policy_gradient/test_VPG.py
@@ -3,10 +3,10 @@ import torch
 import gymnasium as gym
 import sys
 
-from VPG import parse_args
+from VPG import parse_args, setup_device
 
 class TestVPG:
-    def test_parser(self, monkeypatch):
+    def test_parse_args(self, monkeypatch):
         monkeypatch.setattr(sys, "argv", [
             "prog", "--num_envs", "3","--gamma", "0.99", "--learning_rate", "3e-4"
             ])
@@ -15,4 +15,20 @@ class TestVPG:
         assert args.gamma == 0.99
         assert args.rollouts == 500
         assert args.learning_rate == 3e-4
-        
+
+    def test_cuda_available(self, monkeypatch):
+        monkeypatch.setattr(torch.cuda, "is_available", lambda: True)
+        device = setup_device()
+        assert device.type == "cuda"
+
+    def test_mps_availalbe(self, monkeypatch):
+        monkeypatch.setattr(torch.cuda, "is_available", lambda: False)
+        monkeypatch.setattr(torch.backends.mps, "is_available", lambda: True)
+        device = setup_device()
+        assert device.type == "mps"
+
+    def test_cpu_as_device(self, monkeypatch):
+        monkeypatch.setattr(torch.cuda, "is_available", lambda: False)
+        monkeypatch.setattr(torch.backends.mps, "is_available", lambda: False)
+        device = setup_device()
+        assert device.type == "cpu"
